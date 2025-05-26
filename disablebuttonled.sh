4f0d7fc603c73e5e58e4a0f76f8120f9b4705c43
yunshaoyy <2015219629@qq.com>
2025-05-26 23:40:48 +0800

    Add files via upload

---
+++ disablebuttonled.sh
@@ -0,0 +1,4 @@
+echo 0 > /sys/class/leds/mt6370_pmu_led1/brightness
+echo 0 > /sys/class/leds/mt6370_pmu_led2/brightness
+chmod 000 /sys/class/leds/mt6370_pmu_led1/brightness
+chmod 000 /sys/class/leds/mt6370_pmu_led2/brightness
